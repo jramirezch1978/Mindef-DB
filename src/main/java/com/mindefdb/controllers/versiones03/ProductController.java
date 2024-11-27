be00c912de74d82778eb34f6adcf8c1ad8086837
DESKTOP-HBV4KI1\jramirez <jarchiroque@indracompany.com>
2024-11-26 20:25:08 -0500

    clases 2024-11-26

---
+++ src/main/java/com/mindefdb/controllers/versiones03/ProductController.java
@@ -0,0 +1,31 @@
+package com.mindefdb.controllers.versiones03;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+import com.mindefdb.services.IProductService;
+import com.mindefdb.dtos.ProductDTOV1;
+import com.mindefdb.dtos.ProductDTOV2;
+
+@RestController
+@RequestMapping("/api/products")
+public class ProductController {
+    
+	@Autowired
+	private IProductService productService;
+	
+    @GetMapping(produces = "application/vnd.company.app-v1+json")
+    public List<ProductDTOV1> getProductsV1() {
+        return productService.getAllProductsV1();
+    }
+    
+    @GetMapping(produces = "application/vnd.company.app-v2+json")
+    public List<ProductDTOV2> getProductsV2() {
+        return productService.getAllProductsV2();
+    }
+}
